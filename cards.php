d6f1907e64d956730d7a326e2b16255d54cdb545
Barrel-R <henriquebarrel@gmail.com>
2022-06-30 14:27:11 -0300

    Adiciona o front-end

---
+++ cards.php
@@ -0,0 +1,23 @@
+<section class="card-wrapper">
+            <article class="card">
+                <div class="card-content">
+                    <h3 class="card-title">Título da noticia</h3>
+                    <p class="card-description">Descricao da noticia</p>
+                    <button class="card-button">Acessar</button>
+                </div>
+            </article>
+            <article class="card">
+                <div class="card-content">
+                    <h3 class="card-title">Fresh</h3>
+                    <p class="card-description">Noticia sobre novo framework de JavaScript</p>
+                    <button class="card-button">Acessar</button>
+                </div>
+            </article>
+            <article class="card">
+                <div class="card-content">
+                    <h3 class="card-title">Aprovado</h3>
+                    <p class="card-description">Prova DevMedia</p>
+                    <button class="card-button">Acessar</button>
+                </div>            
+            </article>
+    </section>
---
+++ header.php
@@ -0,0 +1,15 @@
+<header class="navbar">
+        <h2 class="logo-title">LOGO</h2>
+        <div class="nav-buttons">
+            <button>
+               <a href="#">Cadastrar noticias</a> 
+            </button>
+            <button>
+              <a href="#">Exibir noticias</a>  
+            </button>
+        </div>
+        <div class="search-bar">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <input class="input-field" type="text">
+        </div>
+    </header>
